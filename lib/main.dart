88d0827eb78a849310cd7661e28af6af67779475
aboanas524 <74088873+aboanas524@users.noreply.github.com>
2022-02-25 16:41:53 +0200

    new model

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:today/database/Database.dart';
 import 'package:today/tasks_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
+
 class _MyAppState extends State<MyApp> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Provider(
       create: (context) => AppDatabase(),
-      child:MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: TasksScreen(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: TasksScreen(),
       ),
     );
   }
